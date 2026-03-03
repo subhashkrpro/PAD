c416155e0c2f3ef8ca68c7d211f827191dddad12
Subhash Kumar <subhash.kr.pro@gmail.com>
2026-03-03 07:05:58 +0000

    Analyzer update

--- src/analyzers/__init__.py
+++ src/analyzers/__init__.py
@@ -2,7 +2,6 @@
 
 from .fft.fft_analyzer import FFTAnalyzer
 from .moire.moire_detector import MoireDetector
-# from .laplacian_variance import LaplacianVarianceAnalyzer
 from .laplacian.laplacian_analyzer import LaplacianVarianceAnalyzer
 from .texture.texture_analyzer import TextureAnalyzer
 
